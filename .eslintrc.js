b467f4194b9a84c3338e2440ac004c7bb3351dd0
Josh <joshgallagher24@gmail.com>
2019-11-14 21:06:02 +0000

    Configure rules for nodejs with ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,15 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
+    rules: {
+        'no-shadow': 'warn',
+        '@typescript-eslint/explicit-member-accessibility': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/indent': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/camelcase': 'off',
+        '@typescript-eslint/array-type': 'off',
+        '@typescript-eslint/no-object-literal-type-assertion': 'off'
+    },
 };
