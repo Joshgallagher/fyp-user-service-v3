6f8d575f829a9816b4567fe1f50dda619b23fd66
Josh <joshgallagher24@gmail.com>
2019-11-15 23:37:53 +0000

    Created 'createUser' RPC controller method

---
+++ src/controller/user.ts
@@ -0,0 +1,11 @@
+import { Context } from 'mali';
+import { getRepository } from 'typeorm';
+import { User } from '../entity/User';
+
+export const createUser = async (context: Context): Promise<void> => {
+    const { name, email, password } = context.request.req;
+
+    const { id } = await getRepository<User>(User).save({ name, email, password });
+
+    context.res = { id, name };
+};
