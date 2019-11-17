91c9cca5f8b1dfdddab2ee64f8e10d826c45bdc8
Josh <joshgallagher24@gmail.com>
2019-11-17 00:38:01 +0000

    Refactored controller logic into a service. Changed terminology from create to register and updated user protofile

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import 'reflect-metadata';
 import Mali from 'mali';
 import { resolve } from 'path';
 import { createConnection } from 'typeorm';
-import { createUser } from './user/user.controller';
+import { registerUser } from './user/user.controller';
 
 const PROTO_PATH = resolve(__dirname, './proto/user.proto');
 
 async function bootstrap() {
     const app = await new Mali(PROTO_PATH, 'UserService');
 
+    app.use({ registerUser });
+
     await createConnection()
         .then(async () => {
-            app.use({ createUser });
-
             await app.start('127.0.0.1:9009');
         })
         .catch(error => console.error(error));
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,32 +1,8 @@
 import { Context } from 'mali';
-import { validateOrReject } from 'class-validator';
-import { getRepository } from 'typeorm';
-import RpcException from 'grpc-error';
-import { status } from 'grpc';
-import { User } from './user.entity';
+import { register } from './user.service';
 
-export const createUser = async (context: Context): Promise<void> => {
-    const { name, email, password } = context.request.req;
-
-    if (!!(await getRepository(User).findOne({ where: { email } }))) {
-        throw new RpcException('VALIDATION_ERROR', status.FAILED_PRECONDITION, {
-            field: 'email',
-            error: 'Email is already in use',
-        });
-    }
-
-    const newUser = User.create({ name, email, password });
-
-    try {
-        await validateOrReject(newUser);
-    } catch ([{ property, constraints }]) {
-        throw new RpcException('VALIDATION_ERROR', status.FAILED_PRECONDITION, {
-            field: property,
-            error: constraints[Object.keys(constraints)[0]],
-        });
-    }
-
-    const { id } = await newUser.save();
+export const registerUser = async (context: Context): Promise<void> => {
+    const { id, name } = await register(context.request.req);
 
     context.res = { id, name };
 };
