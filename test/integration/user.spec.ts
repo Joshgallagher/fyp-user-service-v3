c27fcb4a73544b9e818cff930f97e21448a24659
Josh <joshgallagher24@gmail.com>
2019-11-21 03:42:04 +0000

    Added test to confirm user can not be created with existing email

--- test/integration/user.spec.ts
+++ test/integration/user.spec.ts
@@ -1,6 +1,7 @@
 import { startServer } from '../../src/core/server.core';
 import { User } from '../../src/user/user.entity';
 import { rpcClient } from '../helper/rpc-client.helper';
+import { status as rpcStatus, Metadata } from 'grpc';
 
 describe('A user can register', () => {
     let rpcServer: any;
@@ -11,9 +12,12 @@ describe('A user can register', () => {
         rpcCaller = await rpcClient();
     });
 
+    afterEach(async () => await User.clear());
     afterAll(async () => await rpcServer.close());
 
     test('Successful registration', async () => {
+        expect.assertions(4);
+
         const email = 'test@test.com';
 
         const rpcResponse = await rpcCaller.registerUser({
@@ -29,5 +33,33 @@ describe('A user can register', () => {
         const newUser = await User.find({ where: { email } });
 
         expect(newUser).toHaveLength(1);
+    });
+
+    test('Unsuccessful registration due to unavailable email', async () => {
+        expect.assertions(4);
+
+        const email = 'test@test.com';
+
+        await User.create({ name: 'test', email, password: 'secret' }).save();
+
+        try {
+            await new rpcCaller.Request('registerUser', {
+                name: 'test',
+                email,
+                password: 'secret',
+            })
+                .withResponseMetadata(true)
+                .withResponseStatus(true)
+                .exec();
+        } catch ({ code, details, metadata }) {
+            const responseMetadata = metadata as Metadata;
+
+            expect(code).toEqual(rpcStatus.FAILED_PRECONDITION);
+
+            expect(details).toEqual('VALIDATION_ERROR');
+
+            expect(responseMetadata.get('field')).toContain('email');
+            expect(responseMetadata.get('error')).toContain('Email is already in use');
+        }
     });
 });
