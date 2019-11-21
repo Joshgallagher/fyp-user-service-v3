94840a0c1094307cb3544ea01ce5f82fa9bce443
Josh <joshgallagher24@gmail.com>
2019-11-21 19:31:44 +0000

    Registered auth user handler

--- src/core/server.core.ts
+++ src/core/server.core.ts
@@ -1,6 +1,6 @@
 import Mali from 'mali';
 import { resolve } from 'path';
-import { registerUser } from '../user/user.controller';
+import { registerUser, authenticateUser } from '../user/user.controller';
 import { createConnection } from './connection.core';
 
 const PROTO_PATH = resolve(__dirname, '../proto/user.proto');
@@ -11,7 +11,7 @@ export const startServer = async (): Promise<Mali> => {
 
     const app = new Mali(PROTO_PATH, PROTO_SERVICE);
 
-    app.use({ registerUser });
+    app.use({ registerUser, authenticateUser });
     app.start('127.0.0.1:50052');
 
     return app;
