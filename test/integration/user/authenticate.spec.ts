55212dfa2a481a0ba103b7781e7219b0cf5fcda6
Josh <joshgallagher24@gmail.com>
2019-11-22 02:31:30 +0000

    Added test that cover input validation and credential checking

--- test/integration/user/authenticate.spec.ts
+++ test/integration/user/authenticate.spec.ts
@@ -1,6 +1,7 @@
 import { startServer } from '../../../src/core/server.core';
 import { User } from '../../../src/user/user.entity';
 import { rpcClient } from '../../helper/rpc-client.helper';
+import { status as rpcStatus, Metadata } from 'grpc';
 import faker from 'faker';
 
 describe('A user can authenticate with their email and password', () => {
@@ -33,5 +34,127 @@ describe('A user can authenticate with their email and password', () => {
 
         expect(rpcResponse).toHaveProperty('authenticated');
         expect(rpcResponse).toMatchObject({ authenticated: true });
+    });
+
+    test('Unsuccessful authentication due to email not being provided', async () => {
+        expect.assertions(4);
+
+        const user = {
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(8),
+        };
+
+        await User.create(user).save();
+
+        try {
+            await new rpcCaller.Request('authenticateUser', {
+                email: '',
+                password: user.password,
+            })
+                .withResponseMetadata(true)
+                .withResponseStatus(true)
+                .exec();
+        } catch ({ code, details, metadata }) {
+            const responseMetadata = metadata as Metadata;
+
+            expect(code).toEqual(rpcStatus.FAILED_PRECONDITION);
+
+            expect(details).toEqual('VALIDATION_ERROR');
+
+            expect(responseMetadata.get('field')).toContain('email');
+            expect(responseMetadata.get('error')).toContain('email must be an email');
+        }
+    });
+
+    test('Unsuccessful authentication due to password not being provided', async () => {
+        expect.assertions(4);
+
+        const user = {
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(8),
+        };
+
+        await User.create(user).save();
+
+        try {
+            await new rpcCaller.Request('authenticateUser', {
+                email: user.email,
+                password: '',
+            })
+                .withResponseMetadata(true)
+                .withResponseStatus(true)
+                .exec();
+        } catch ({ code, details, metadata }) {
+            const responseMetadata = metadata as Metadata;
+
+            expect(code).toEqual(rpcStatus.FAILED_PRECONDITION);
+
+            expect(details).toEqual('VALIDATION_ERROR');
+
+            expect(responseMetadata.get('field')).toContain('password');
+            expect(responseMetadata.get('error')).toContain('password must be longer than or equal to 6 characters');
+        }
+    });
+
+    test('Unsuccessful authentication due to incorrect user credentials (email)', async () => {
+        expect.assertions(3);
+
+        const user = {
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(8),
+        };
+
+        await User.create(user).save();
+
+        try {
+            await new rpcCaller.Request('authenticateUser', {
+                email: 'incorrectemail@test.com',
+                password: user.password,
+            })
+                .withResponseMetadata(true)
+                .withResponseStatus(true)
+                .exec();
+        } catch ({ code, details, metadata }) {
+            const responseMetadata = metadata as Metadata;
+
+            expect(code).toEqual(rpcStatus.UNAUTHENTICATED);
+
+            expect(details).toEqual('UNAUTHENTICATED_ERROR');
+
+            expect(responseMetadata.get('error')).toContain('The provided email or password are incorrect');
+        }
+    });
+
+    test('Unsuccessful authentication due to incorrect user credentials (password)', async () => {
+        expect.assertions(3);
+
+        const user = {
+            name: faker.name.firstName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(8),
+        };
+
+        await User.create(user).save();
+
+        try {
+            await new rpcCaller.Request('authenticateUser', {
+                email: user.email,
+                password: 'wrongPassword123',
+            })
+                .withResponseMetadata(true)
+                .withResponseStatus(true)
+                .exec();
+        } catch ({ code, details, metadata }) {
+            const responseMetadata = metadata as Metadata;
+
+            expect(code).toEqual(rpcStatus.UNAUTHENTICATED);
+
+            expect(details).toEqual('UNAUTHENTICATED_ERROR');
+
+            expect(responseMetadata.get('error')).toContain('The provided email or password are incorrect');
+        }
     });
 });
