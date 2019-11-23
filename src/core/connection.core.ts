9e96177978c9a5ac43488fbc52129168d7f780cd
Josh <joshgallagher24@gmail.com>
2019-11-23 19:24:27 +0000

    Moved core typeorm config into connection file config

--- src/core/connection.core.ts
+++ src/core/connection.core.ts
@@ -3,5 +3,11 @@ import { createConnection as connect, getConnectionOptions } from 'typeorm';
 export const createConnection = async (): Promise<void> => {
     const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
 
-    await connect({ ...connectionOptions, name: 'default' });
+    await connect({
+        ...connectionOptions,
+        name: 'default',
+        entities: ['src/**/*.entity.ts'],
+        migrations: ['src/database/migration/**/*.ts'],
+        subscribers: ['src/**/*.subscriber.ts'],
+    });
 };
