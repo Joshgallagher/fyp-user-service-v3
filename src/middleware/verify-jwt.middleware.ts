ee3648ba69effa1e5d47d3c0657b514ff235d460
Josh <joshgallagher24@gmail.com>
2019-12-10 19:43:24 +0000

    Added error handling and configured error responses to use RPC errors with metadata

--- src/middleware/verify-jwt.middleware.ts
+++ src/middleware/verify-jwt.middleware.ts
@@ -1,16 +1,45 @@
 import { Context } from 'mali';
 import bearer from '@malijs/bearer';
-import { JWKS, JWT } from 'jose';
+import { JWKS, JWT, errors } from 'jose';
 import { get } from 'request-promise-native';
+import RpcException from 'grpc-error';
+import { status } from 'grpc';
 
 export const verifyJwtMiddleware = (): Function => {
     return bearer(
         async (token: string, _context: Context, next: any): Promise<void> => {
-            const body = await get(`${process.env.OATHKEEPER_API_URL}/.well-known/jwks.json`);
+            let jwks: any;
 
-            const ks = JWKS.asKeyStore(JSON.parse(body));
+            try {
+                jwks = await get(`${process.env.OATHKEEPER_API_URL}/.well-known/jwks.json`);
+            } catch (e) {
+                throw new RpcException('INTERNAL_ERROR', status.INTERNAL, {
+                    error: 'Something went wrong when requesting the JWKS from Oathkeeper API',
+                });
+            }
 
-            JWT.verify(token, ks, {});
+            try {
+                const ks = JWKS.asKeyStore(JSON.parse(jwks));
+                JWT.verify(token, ks, {});
+            } catch (e) {
+                const code = e.code.substring(4);
+                const clientError =
+                    (e as any) instanceof
+                    (errors.JWTMalformed ||
+                        errors.JWTClaimInvalid ||
+                        errors.JWSVerificationFailed ||
+                        errors.JWSInvalid);
+
+                if (clientError) {
+                    throw new RpcException(code, status.FAILED_PRECONDITION, {
+                        error: e.message,
+                    });
+                }
+
+                throw new RpcException(code, status.INTERNAL, {
+                    error: e.message,
+                });
+            }
 
             await next();
         },
